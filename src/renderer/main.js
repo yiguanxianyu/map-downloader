0da2d3458b58b0beebd9411cc9c39dd1f134dc62
yiguanxianyu <yiguanxianyu@gmail.com>
2024-02-06 10:58:08 +0800

    update deps

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,13 +1,10 @@
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import App from './App.vue'
 
-const pinia = createPinia()
 const app = createApp(App)
 
 app.use(ElementPlus)
-app.use(pinia)
 app.mount('#app')
